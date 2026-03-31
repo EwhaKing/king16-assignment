e4657595ee4b4095b844965f550176e7b37448bd
Daeun-webp <sireha.ovo@gmail.com>
2026-03-31 15:08:47 +0900

    Unity 스터디 1주차 파일 추가

---
+++ PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("콘솔 출력 테스트 문장");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
